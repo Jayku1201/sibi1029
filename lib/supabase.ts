f6f74cd135ef068e804526c2502540dedb3d704c
JayKu <jayku1201@gmail.com>
2025-10-31 20:16:25 -0700

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabaseBrowser = () =>
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+
+// Only to be used server-side for privileged operations.
+export const supabaseServer = () =>
+  createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    { auth: { persistSession: false } }
+  )
